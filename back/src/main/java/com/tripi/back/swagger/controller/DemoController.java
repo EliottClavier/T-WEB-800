a318845690cb621ceaab6f11c5bcd039f6af3492
paulripault <paulripaut0@gmail.com>
2023-03-06 12:34:09 +0100

    [ADD] swagger configuration

---
+++ back/src/main/java/com/tripi/back/swagger/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.tripi.back.swagger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class DemoController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {return "Hello World!";}
+}
