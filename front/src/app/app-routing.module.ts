e191eb973a16e6b7c88e77f753484628392fbe56
Nathan-Pignon <nathan.pignon@epitech.eu>
2023-03-13 14:57:31 +0100

    [WEB-230] Update tree structure

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { RegisterUserComponent } from './containers/register-user/register-user.component';
 import {MainComponent} from "./layouts/main/main.component";
 
 const routes: Routes = [
