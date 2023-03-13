6a1b5340b8ec1d1807a8471eecf0c6fe16e83892
Clavier Eliott <eliott.clavier@epitech.eu>
2023-03-13 14:27:59 +0100

    [WEB-148] REsolve conflicts

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
-import { RegisterUserComponent } from './components/register-user/register-user.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {MatButtonModule} from "@angular/material/button";
