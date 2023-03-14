af65cb2743a753efddc38c06eab076c0c4580aa5
Nathan-Pignon <nathan.pignon@epitech.eu>
2023-03-14 16:47:26 +0100

    [WEB-231] Add e2e tests for login service

--- front/src/app/enums/api-response-const.ts
+++ front/src/app/enums/api-response-const.ts
@@ -1,5 +1,6 @@
 export class ApiResponseConst {
   INFO_MESSAGES = {
     BAD_REQUEST: 'Error 400 : An error has occurred, please try again later',
+    BAD_CREDENTIALS: 'Error 401 : Bad credentials',
   };
 }
