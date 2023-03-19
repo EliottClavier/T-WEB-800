9f82430d80751f9c057840b30ca0da28f9b01a33
Clemathe <clement.mathe@rdtingenieurs.fr>
2023-03-19 12:45:32 +0100

    [WEB-176] Add getSuggestions in http Service provider

--- front/src/app/services/suggestions-service/suggestions.service.spec.ts
+++ front/src/app/services/suggestions-service/suggestions.service.spec.ts
@@ -1,19 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SuggestionsService } from './suggestions.service';
+import {createServiceFactory, Spectator, SpectatorService} from "@ngneat/spectator";
+import {createComponent, inject, ProviderToken} from "@angular/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {T} from "@angular/cdk/keycodes";
+import {TranslateService} from "@ngx-translate/core";
+import {AppModule} from "../../app.module";
 
 describe('SuggestionsService', () => {
-  let service: SuggestionsService;
 
-
+  let spectator: SpectatorService<SuggestionsService>;
+  let httpclient: HttpClient;
+  const createService = createServiceFactory({
+    service: SuggestionsService,
+    imports: [HttpClientModule],
+    providers: [HttpClient],
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SuggestionsService);
+    spectator = createService();
+    httpclient  = spectator.inject(HttpClient);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(spectator.service).toBeTruthy();
+  });
+
+  it('should inject HttpClient', () => {
+    // const httpclient = spectator.inject(HttpClient);
+    expect(httpclient).toBeDefined();
+  });
+
+  it('should have HttpClient as dependency', () => {
+    const myService = spectator.inject(HttpClient);
+    expect(myService).toBeDefined();
   });
 
 });
--- front/src/app/services/suggestions-service/suggestions.service.ts
+++ front/src/app/services/suggestions-service/suggestions.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ItemModel} from "../../models/item/item.model";
+import {ItemType} from "../../models/ItemType";
+import {HttpClient} from "@angular/common/http";
+import {SuggestionsStoreService} from "../../store/suggestions-store.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class SuggestionsService  {
+export class SuggestionsService {
 
-  constructor() { }
+  private suggestions_url: string = 'http://localhost:3000/suggestions';
 
-  //  getSuggestions() {
-  //   return new Array<ItemModel>();
-  // }
+
+  constructor(private _httpclient: HttpClient, private suggestionStore: SuggestionsStoreService) {
+
+  }
+
+  getSuggestions(itemType: ItemType): void {
+    this._httpclient.get('suggestions_url' + '/itemType').subscribe({
+
+      next: (data ) => {
+        this.suggestionStore.suggestions.next(data as ItemModel[]);
+      },
+
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('complete');
+
+      }
+    });
+  }
 }
