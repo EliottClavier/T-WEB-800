a14b039f8537f63dda3009a815e95438410528bb
Clemathe <clement.mathe@rdtingenieurs.fr>
2023-03-20 09:53:27 +0100

    [WEB-176] Add getSuggestions in http Service provider

--- front/src/app/services/suggestions-service/suggestions.service.spec.ts
+++ front/src/app/services/suggestions-service/suggestions.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 
 import { SuggestionsService } from './suggestions.service';
 import {createServiceFactory, Spectator, SpectatorService} from "@ngneat/spectator";
@@ -7,6 +7,19 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {T} from "@angular/cdk/keycodes";
 import {TranslateService} from "@ngx-translate/core";
 import {AppModule} from "../../app.module";
+import {ItemModel} from "../../models/item/item.model";
+import {ItemType} from "../../models/ItemType";
+import {of} from "rxjs";
+
+function getBarItems() {
+  let data = new Array<ItemModel>();
+  for (let i = 0; i < 3; i++) {
+    let item = new ItemModel();
+    item.typeOfItem = ItemType.BAR;
+    data.push(item);
+  }
+  return data;
+}
 
 describe('SuggestionsService', () => {
 
@@ -36,5 +49,21 @@ describe('SuggestionsService', () => {
     const myService = spectator.inject(HttpClient);
     expect(myService).toBeDefined();
   });
+  it('On init users should be loaded', fakeAsync(() => {
+    const myService = spectator.inject(HttpClient);
+    // spyOn(SuggestionsService, 'getSuggestions').and.returnValue(of(getBarItems()));
 
+    // Trigger ngOnInit()
+    // myService.detectChanges();
+
+    // expect(component.loading).toBeTruthy();
+    // expect(component.users).toBeUndefined();
+    // expect(userService.getUsers).toHaveBeenCalledWith();
+    //
+    // // Simulates the asynchronous passage of time
+    // tick(1);
+    //
+    // expect(component.loading).toBeFalsy();
+    // expect(component.users).toEqual([user]);
+  }));
 });
