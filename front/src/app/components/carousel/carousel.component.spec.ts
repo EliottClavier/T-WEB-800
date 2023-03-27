5622a0902a51bf90bd4efc0d0542f280fb0581d6
paulripault <paulripaut0@gmail.com>
2023-03-27 16:16:52 +0200

    [WEB-255] adding test for multiple images & cleaning code

--- front/src/app/components/carousel/carousel.component.spec.ts
+++ front/src/app/components/carousel/carousel.component.spec.ts
@@ -1,20 +1,16 @@
 import { CarouselComponent } from './carousel.component'
 import { createComponentFactory, Spectator } from "@ngneat/spectator";
-import { ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { NgbCarousel, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Image } from 'src/app/types/image.types';
 
 describe('CarouselComponent', () => {
   let component: CarouselComponent;
   let fixture: ComponentFixture<CarouselComponent>;
-
   let spectator: Spectator<CarouselComponent>;
   let wrongImages: Image[];
 
-  const createComponent = createComponentFactory({
-    component: CarouselComponent,
-  });
+  const createComponent = createComponentFactory({ component: CarouselComponent });
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -27,17 +23,11 @@ describe('CarouselComponent', () => {
     fixture = TestBed.createComponent(CarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
     spectator = createComponent();
     component = spectator.component;
 
-    component.images = [
-      { src: 'https://picsum.photos/id/28/4928/3264', alt: 'img' },
-      { src: 'https://picsum.photos/id/42/3456/2304', alt: 'image2' },
-      { src: 'https://picsum.photos/id/45/4592/2576', alt: 'img' }
-    ];
-
-    wrongImages = [{ src: 'https://picsum.photos/id/45/4592/2576', alt: 'default-wrong' }];
+    component.images = [];
+    wrongImages = [{ src: 'https://picsum.photos/id/1084/4579/3271', alt: 'default-wrong' }];
 
     spectator.detectChanges();
   });
@@ -46,8 +36,9 @@ describe('CarouselComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should return an empty array at the initialisation', () => { });
-  it('should return image from api in good format', () => { });
+  it('should have an empty array at the initialisation', () => {
+    expect(component.images).toEqual([]);
+  });
 
   it('should render the carousel', () => {
     fixture.detectChanges();
@@ -56,6 +47,12 @@ describe('CarouselComponent', () => {
   });
 
   it('should update the images when the array changes', () => {
+    component.images.push(
+      { src: 'https://picsum.photos/id/28/4928/3264', alt: 'img' },
+      { src: 'https://picsum.photos/id/42/3456/2304', alt: 'image2' },
+      { src: 'https://picsum.photos/id/45/4592/2576', alt: 'img' }
+    );
+
     fixture.detectChanges();
     const imageElements = fixture.nativeElement.querySelectorAll('img');
     expect(imageElements.length).toBe(3);
@@ -65,16 +62,6 @@ describe('CarouselComponent', () => {
     expect(imageElements[1].alt).toContain(component.images[1].alt);
     expect(imageElements[2].src).toContain(component.images[2].src);
   });
-
-  //TODO ➝ check when API is functionnal
-  // it('should not render the photo information if wrong', () => {
-  //   fixture.detectChanges();
-  //   const imageElements = fixture.nativeElement.querySelectorAll('.imageNotFound');
-  //   expect(imageElements.length).toBe(1);
-
-  //   expect(imageElements[0].src).toContain(wrongImages[0].src);
-  //   expect(imageElements[0].alt).toContain(wrongImages[0].alt);
-  // });
 });
 
 function getSlideElements(nativeElement: any) {
--- front/src/app/components/carousel/carousel.component.ts
+++ front/src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Image } from 'src/app/types/image.types';
 
 @Component({
@@ -7,28 +7,24 @@ import { Image } from 'src/app/types/image.types';
   styleUrls: ['./carousel.component.css'],
 })
 
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
   public showNavigationArrows: boolean = false;
   public showNavigationIndicators: boolean = false;
-
   public images: Image[] = [
     { src: 'https://picsum.photos/id/28/4928/3264', alt: 'img' },
     { src: 'https://picsum.photos/id/42/3456/2304', alt: 'image2' },
     { src: 'https://picsum.photos/id/45/4592/2576', alt: 'img' }
   ];
+  public wrongImage: Image = { src: 'https://picsum.photos/id/1084/4579/3271', alt: 'default-wrong' }
 
-  public wrongImage: Image = { src: 'https://picsum.photos/id/45/4592/2576', alt: 'default-wrong' }
+  ngOnInit() {
+    this.getApiImages();
+  }
 
-  //! Update when API in place
-  getImagesFromAPI() {
+  getApiImages() {
     //call api
     //return 3 random images
     //initialise images
-    //check if image is not null
-    this.images.push(
-      { src: 'https://picsum.photos/id/28/4928/3264', alt: 'img' },
-      { src: 'https://picsum.photos/id/42/3456/2304', alt: 'image2' },
-      { src: 'https://picsum.photos/id/45/4592/2576', alt: 'img' }
-    );
+    //check if image is not null => push wrongImage if null
   }
 }
