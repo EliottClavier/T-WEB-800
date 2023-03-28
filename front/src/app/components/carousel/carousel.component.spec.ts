c22b2a26de3d97e2e6d64119a4686ae7edcb68e4
paulripault <paulripaut0@gmail.com>
2023-03-28 11:35:47 +0200

    [WEB-255] update image

--- front/src/app/components/carousel/carousel.component.spec.ts
+++ front/src/app/components/carousel/carousel.component.spec.ts
@@ -27,7 +27,7 @@ describe('CarouselComponent', () => {
     component = spectator.component;
 
     component.images = [];
-    wrongImages = [{ src: 'https://picsum.photos/id/1084/4579/3271', alt: 'default-wrong' }];
+    wrongImages = [{ src: 'https://c.pxhere.com/photos/0f/d8/mailbox_404_numbers_lettering-1076506.jpg!d', alt: 'default-wrong' }];
 
     spectator.detectChanges();
   });
@@ -48,9 +48,9 @@ describe('CarouselComponent', () => {
 
   it('should update the images when the array changes', () => {
     component.images.push(
-      { src: 'https://picsum.photos/id/28/4928/3264', alt: 'img' },
-      { src: 'https://picsum.photos/id/42/3456/2304', alt: 'image2' },
-      { src: 'https://picsum.photos/id/45/4592/2576', alt: 'img' }
+      { src: 'https://c.pxhere.com/photos/f7/55/namdaemun_market_myeongdong_seoul_korea_south_korean_city-1104854.jpg!d', alt: 'img' },
+      { src: 'https://c.pxhere.com/photos/b1/c5/building_balcony_apartment_neighborhood_residential-99128.jpg!d', alt: 'image2' },
+      { src: 'https://c.pxhere.com/photos/37/c4/alley_building_old_city_street-173743.jpg!d', alt: 'img' }
     );
 
     fixture.detectChanges();
--- front/src/app/components/carousel/carousel.component.ts
+++ front/src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Image } from 'src/app/types/image.type';
 
 @Component({
@@ -7,24 +7,13 @@ import { Image } from 'src/app/types/image.type';
   styleUrls: ['./carousel.component.css'],
 })
 
-export class CarouselComponent implements OnInit {
+export class CarouselComponent {
   public showNavigationArrows: boolean = false;
   public showNavigationIndicators: boolean = false;
   public images: Image[] = [
-    { src: 'https://picsum.photos/id/28/4928/3264', alt: 'img' },
-    { src: 'https://picsum.photos/id/42/3456/2304', alt: 'image2' },
-    { src: 'https://picsum.photos/id/45/4592/2576', alt: 'img' }
+    { src: 'https://c.pxhere.com/photos/f7/55/namdaemun_market_myeongdong_seoul_korea_south_korean_city-1104854.jpg!d', alt: 'img' },
+    { src: 'https://c.pxhere.com/photos/b1/c5/building_balcony_apartment_neighborhood_residential-99128.jpg!d', alt: 'image2' },
+    { src: 'https://c.pxhere.com/photos/37/c4/alley_building_old_city_street-173743.jpg!d', alt: 'img' }
   ];
-  public wrongImage: Image = { src: 'https://picsum.photos/id/1084/4579/3271', alt: 'default-wrong' }
-
-  ngOnInit() {
-    this.getApiImages();
-  }
-
-  getApiImages() {
-    //call api
-    //return 3 random images
-    //initialise images
-    //check if image is not null => push wrongImage if null
-  }
+  public wrongImage: Image = { src: 'https://c.pxhere.com/photos/0f/d8/mailbox_404_numbers_lettering-1076506.jpg!d', alt: 'default-wrong' }
 }
