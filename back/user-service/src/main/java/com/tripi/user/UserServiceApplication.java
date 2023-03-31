f2659f5b283d1a581c9ec3dde41e6794f237712d
Clavier Eliott <eliott.clavier@epitech.eu>
2023-04-01 00:23:11 +0200

    [WEB-257] Fix auth and user services

--- back/user-service/src/main/java/com/tripi/user/UserServiceApplication.java
+++ back/user-service/src/main/java/com/tripi/user/UserServiceApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,7 +15,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Import({ WebSecurityConfiguration.class })
 @EnableJpaRepositories(basePackages = "com.tripi.user.repository")
 @EnableDiscoveryClient
-public class UserServiceApplication {
+public class UserServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
