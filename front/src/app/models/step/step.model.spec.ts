3fe66c3d980ac58f53bd97c4f935a737402c9584
Clemathe <clement.mathe@rdtingenieurs.fr>
2023-04-01 23:17:31 +0200

    [WEB-133] uuid + test

--- front/src/app/models/step/step.model.spec.ts
+++ front/src/app/models/step/step.model.spec.ts
@@ -1,11 +1,12 @@
-import {LeisureItemModel} from "../leisures/leisure-item.model";
 import {StepModel} from "./step.model";
 import {LocationModel} from "../location/location.model";
 import {getIsoStringFromDate} from "../../utils/date.utils";
-
+import {getAccommodationItems, getRestaurantItems} from "../../utils/suggestions-mock.utils";
+import TravelMode = google.maps.TravelMode;
 
 
 describe('StepModel', () => {
+
   let step: StepModel;
 
   beforeEach(() => {
@@ -15,5 +16,59 @@ describe('StepModel', () => {
   it('should create a new step', () => {
     expect(step).toBeTruthy();
   });
+  it('should set and get the id property', () => {
+    expect(step.id.length).toEqual(36);
+    step.id = '2';
+    expect(step.id).toEqual('2');
+  });
+  it('should set and get the name property', () => {
+
+    expect(step.name).toEqual('');
+    step.name = 'Lyon';
+    expect(step.name).toEqual('Lyon');
+  });
+
+  it('should set and get the location property', () => {
+    let location = step.location;
+    expect(location).toEqual(new LocationModel("", "", 0, 0));
+    let nantes = new LocationModel("2", "Nantes", 0, 0);
+    step.location = nantes;
+    expect(step.location).toEqual(nantes);
+  });
+
+  it('should set and get the start property', () => {
+    expect(step.start).toEqual(getIsoStringFromDate(new Date()));
+    step.start = getIsoStringFromDate(new Date());
+    expect(step.start).toEqual(getIsoStringFromDate(new Date()));
+  });
+
+  it('should set and get the end property', () => {
+    let end = step.end;
+    expect(step.end).toEqual(end);
+    step.end = getIsoStringFromDate(new Date());
+    expect(step.end).toEqual(getIsoStringFromDate(new Date()));
+  });
+
+  it('should set and get the leisure property', () => {
+    let leisure = getAccommodationItems()
+    step.leisures = leisure;
+    expect(step.leisures).toEqual(leisure);
+
+    step.leisures = getRestaurantItems();
+    expect(step.leisures).toEqual(getRestaurantItems());
+  });
+
+  it('should set and get the travelMode property', () => {
+    expect(step.travelMode).toEqual(undefined);
+    step.travelMode = TravelMode.WALKING;
+    expect(step.travelMode).toEqual(TravelMode.WALKING);
+  });
+
+  it('should set and get the index property', () => {
+    expect(step.index).toEqual(0);
+    step.index = 1;
+    expect(step.index).toEqual(1);
+  });
+
 
 });
--- front/src/app/models/trip/trip.model.spec.ts
+++ front/src/app/models/trip/trip.model.spec.ts
@@ -1,4 +1,5 @@
 import {TripModel} from "./trip.model";
+import {getIsoStringFromDate} from "../../utils/date.utils";
 
 describe('TripModel', () => {
   let trip: TripModel;
@@ -10,5 +11,47 @@ describe('TripModel', () => {
   it('should create a new trip', () => {
     expect(trip).toBeTruthy();
   });
+
+  it('should set and get the id property', () => {
+    expect(trip.id.length).toEqual(36);
+    trip.id = '2';
+    expect(trip.id).toEqual('2');
+  });
+
+  it('should set and get the name property', () => {
+    expect(trip.name).toEqual('');
+    trip.name = 'myTrip';
+    expect(trip.name).toEqual('myTrip');
+  } );
+
+
+  it('should set and get the start property', () => {
+    let start = trip.startDate;
+    expect(trip.startDate).toEqual(start);
+    trip.startDate = getIsoStringFromDate(new Date());
+    expect(trip.startDate).toEqual(getIsoStringFromDate(new Date()));
+  });
+
+  it('should set and get the end property', () => {
+    let end = trip.endDate;
+    expect(trip.endDate).toEqual(end);
+    trip.endDate = getIsoStringFromDate(new Date());
+    expect(trip.endDate).toEqual(getIsoStringFromDate(new Date()));
+  } );
+
+  it('should set and get the steps property', () => {
+    let steps = trip.steps;
+    expect(trip.steps).toEqual(steps);
+    trip.steps = [];
+    expect(trip.steps).toEqual([]);
+  });
+
+  it('should set and get the isSaved property', () => {
+    let isSaved = trip.isSaved;
+    expect(trip.isSaved).toEqual(isSaved);
+    trip.isSaved = true;
+    expect(trip.isSaved).toEqual(true);
+  });
+
 
 });
