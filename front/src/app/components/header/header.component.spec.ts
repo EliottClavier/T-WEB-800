04555f2e31d5163d9ac6c24f1600b421de1ca76e
paulripault <paulripaut0@gmail.com>
2023-04-03 21:14:15 +0200

    [WEB-258] formatting code

--- front/src/app/components/header/header.component.spec.ts
+++ front/src/app/components/header/header.component.spec.ts
@@ -56,23 +56,4 @@ describe('HeaderComponent', () => {
     expect(component.openLoginDialog).toHaveBeenCalled();
     expect(component["_dialog"].open).toHaveBeenCalled();
   });
-
-  // it('should open register dialog on click', () => {
-  //   spyOn<HeaderComponent, any>(component, 'openRegisterDialog').and.callThrough();
-  //   spyOn<MatDialog, any>(component["_dialog"], 'open').and.callThrough();
-  //   component.openRegisterDialog();
-  //   spectator.detectChanges();
-  //   expect(component.openRegisterDialog).toHaveBeenCalled();
-  //   expect(component["_dialog"].open).toHaveBeenCalled();
-  // });
-
-  // it('should open register dialog', () => {
-  //   spyOn<HeaderComponent, any>(component, 'openRegisterDialog').and.callThrough();
-  //   spyOn<MatDialog, any>(component["_dialog"], 'open').and.callThrough();
-  //   spectator.click('[header-register] button');
-  //   spectator.detectChanges();
-  //   expect(component.openRegisterDialog).toHaveBeenCalled();
-  //   expect(component["_dialog"].open).toHaveBeenCalled();
-  // });
-
 })
